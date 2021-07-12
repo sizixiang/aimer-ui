626b4054e456b90e1a98ed6061b343125ea44ed8
苏子翔 <1092453305@qq.com>
2021-07-12 23:16:50 +0800

    改用less,采用jsx写法

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import AimerUI from '../packages'
+import '../packages/style'
 
 const app = createApp(App)
 app.use(AimerUI)
---
+++ packages/button/src/button.jsx
@@ -0,0 +1,9 @@
+export default{
+    name: 'AimerButton',
+    props: ['type'],
+    render() {
+        return (
+            <button type="button" class="aimer-btn">测试按钮</button>
+        )
+    }
+}
